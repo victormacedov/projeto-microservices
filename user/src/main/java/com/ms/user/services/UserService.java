925ec198d7bb9dae71613b8f650f57cda476f44f
Victor Macedo <victormacedo1001@gmail.com>
2024-08-12 20:42:16 -0300

    Implementation of User Producer

--- user/src/main/java/com/ms/user/services/UserService.java
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ms.user.services;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
 import com.ms.user.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -8,13 +9,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     final UserRepository userRepository;
+    final UserProducer userProducer;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
     @Transactional
     public UserModel save(UserModel userModel) {
-        return userRepository.save(userModel);
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
     }
 }
