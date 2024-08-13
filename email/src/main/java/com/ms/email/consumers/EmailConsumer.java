820441d43add84009cce420abdbb992a1f7505d7
Victor Macedo <victormacedo1001@gmail.com>
2024-08-12 21:51:26 -0300

    Implementing sending emails with SMTP Gmail

--- email/src/main/java/com/ms/email/consumers/EmailConsumer.java
+++ email/src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -2,6 +2,7 @@ package com.ms.email.consumers;
 
 import com.ms.email.dtos.EmailRecordDto;
 import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -9,9 +10,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
+    final EmailService emailService;
+
+    public EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto){
         var emailModel = new EmailModel();
         BeanUtils.copyProperties(emailRecordDto, emailModel);
+
+        emailService.sendEmail(emailModel);
     }
 }
